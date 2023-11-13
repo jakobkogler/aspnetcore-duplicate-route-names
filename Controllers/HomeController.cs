0bc66d3d669fc0a659fee4bbffc7c54341001c79
Jakob Kogler <jakob.kogler@gmail.com>
2023-11-13 11:38:38 +0100

    duplicate route name

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace aspnet.Controllers;
+
+public class HomeController : Controller
+{
+    [HttpGet("/method", Name = nameof(GetMethod))]
+    public string GetMethod()
+    {
+        return "GET";
+    }
+
+    [HttpPost("/method", Name = nameof(GetMethod))]
+    public string PostMethod()
+    {
+        return "POST";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
